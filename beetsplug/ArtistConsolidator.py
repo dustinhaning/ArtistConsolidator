e79210a2672d22a83af431a9976145802e0cfffb
dustinhaning <57005114+dustinhaning@users.noreply.github.com>
2023-07-01 01:32:37 -0500

    Add files via upload

--- beetsplug/ArtistConsolidator.py
+++ beetsplug/ArtistConsolidator.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # File: beetsplug/ArtistConsolidator.py
 from beets.plugins import BeetsPlugin
 from beets.importer import action
@@ -6,17 +7,20 @@ from beets.ui import Subcommand
 class ArtistConsolidatorPlugin(BeetsPlugin):
     def __init__(self):
         super(ArtistConsolidatorPlugin, self).__init__()
-        self.config.add({
-            'mappings': []
-        })
-        self.import_stages = [self.imported]
-        self.register_listener('import_task_choice', self.modify)
 
-    def modify(self, session, task):
-        mappings = self.config['mappings'].as_pairs()
-        if task.choice_flag == action.APPLY:
-            for item in task.items:
-                for original_names, new_name in mappings:
-                    if item.albumartist in original_names:
-                        item.albumartist = new_name
-                        item.store()
+        self.register_listener('import_task_choice', self.consolidate_artists)
+
+    def commands(self):
+        cmd = Subcommand('consolidate_artists',
+                         help='rename artist variants to a single name')
+        return [cmd]
+
+    def consolidate_artists(self, session, task):
+        if task.choice_flag is action.ASIS:
+            return
+
+        artist_name_mapping = self.config['artist_mapping'].get(dict)
+        for key in artist_name_mapping:
+            if task.albumartist == key:
+                task.albumartist = artist_name_mapping[key]
+                self._log.info(u'Changed album artist to: {0}', task.albumartist)
