74241da4e081159cab9d30e120fd755924e0bc92
dustinhaning <57005114+dustinhaning@users.noreply.github.com>
2023-07-01 13:44:45 -0500

    Add files via upload
    Added option to update artist field for each track to match album artist

--- beetsplug/ArtistConsolidator.py
+++ beetsplug/ArtistConsolidator.py
@@ -8,9 +8,11 @@ class ArtistConsolidatorPlugin(BeetsPlugin):
     def __init__(self):
         super(ArtistConsolidatorPlugin, self).__init__()
         self.config.add({
-            'artist_dict': {}
+            'artist_dict': {},
+            'update_track_artists': False  # Default option set to False
         })
-        self.artist_dict = self.config['artist_dict']
+        self.artist_dict = self.config['artist_dict']  # Use it directly
+        self.update_track_artists = self.config['update_track_artists'].get(bool)
 
         self.register_listener('import_task_choice', self.consolidate_artists)
 
@@ -26,14 +28,16 @@ class ArtistConsolidatorPlugin(BeetsPlugin):
                     if item.albumartist == original:
                         log.info(f"Changing albumartist from {original} to {consolidated}")
                         item.albumartist = consolidated
-                        item.artist = consolidated
+                        if self.update_track_artists:
+                            item.artist = consolidated  # Update track artist only if option is set to True
                         item.store()
         else:
-            # If it's a singleton, update the albumartist and artist fields for the item only.
+            # If it's a singleton, update the albumartist fields for the item only.
             for item in task.items:
                 for original, consolidated in self.artist_dict.items():
                     if item.albumartist == original:
                         log.info(f"Changing albumartist from {original} to {consolidated}")
                         item.albumartist = consolidated
-                        item.artist = consolidated 
+                        if self.update_track_artists:
+                            item.artist = consolidated  # Update track artist only if option is set to True
                         item.store()
